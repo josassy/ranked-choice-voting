1453c0fd32b6491612f13684e910d575c8a0c29d
Josiah Lansford <39782615+josassy@users.noreply.github.com>
2021-11-23 23:25:22 -0500

    style an h1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom';
 
 const Home: React.FC = props => (
   <div className="flex flex-wrap">
-    <div className="bg-secondary w-full md:w-1/2 md:h-screen">
-      <h1>Ranked Choice Voting</h1>
+    <div className="bg-secondary w-full md:w-1/2 md:h-screen p-8">
+      <h1 className="font-sans pt-40 pb-10 text-5xl font-black">Ranked Choice Voting</h1>
+      <p>I'm baby vexillologist gochujang blue bottle art party PBR&B unicorn. Blog forage DIY fingerstache chambray. Mumblecore offal waistcoat tacos farm-to-table shaman intelligentsia tousled trust fund. Meh scenester biodiesel, quinoa prism flannel jianbing VHS.</p>
     </div>
     <div className="bg-primary w-full md:w-1/2 md:h-screen">
       <Link to="/vote">Vote</Link>
